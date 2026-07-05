3198e26a478f6819ebc919af2eb269222140bd05
Fahima Azizi <fahimaazizi397@gmail.com>
2026-07-05 23:52:07 +0500

    update

--- client/src/pages/MyGeneration.tsx
+++ client/src/pages/MyGeneration.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import ProjectCard from "../components/ProjectCard";
-import PrimaryButton from "../components/PrimaryButton";
+import { PrimaryButton } from "../components/Buttons";
 import type { Project } from "../types";
 
 const MyGenerations = () => {
