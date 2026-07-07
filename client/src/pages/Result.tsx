f29b07eb849b606c482b1a4914cb8f3b26c1e258
Fahima Azizi <fahimaazizi397@gmail.com>
2026-07-07 23:13:27 +0500

    update

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import type { Project } from "../types";
 
 const Result = () => {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
 
   const [project, setProjectData] = useState<Project>({} as Project);
   const [loading, setLoading] = useState(true);
@@ -29,11 +29,23 @@ const Result = () => {
     fetchProjectData();
   }, []);
 
-  return (
-    <div>
-      {/* Your Result UI */}
+return loading ? (
+  <div className="h-screen w-full flex items-center justify-center">
+    <Loader2Icon className="animate-spin text-indigo-500 size-9" />
+  </div>
+) : (
+  <div className="min-h-screen text-white p-6 md:p-12 mt-20">
+    <div className="max-w-6xl mx-auto">
+      <header className="flex items-center justify-between mb-8">
+        <div>
+          <h1 className="text-4xl font-bold">Generation Result</h1>
+          <p className="text-gray-400 mt-2">{project.productName}</p>
+        </div>
+
+      </header>
     </div>
-  );
+  </div>
+);
 };
 
 export default Result;
