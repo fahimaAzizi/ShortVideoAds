4154f3281cb924fab11ab5e1eaf18d4a5080ee15
Fahima Azizi <fahimaazizi397@gmail.com>
2026-07-15 19:41:29 +0500

    update

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
