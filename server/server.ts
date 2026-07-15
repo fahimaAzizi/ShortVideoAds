bc890b3cdd1878d68b2800244c542da149639315
Fahima Azizi <fahimaazizi397@gmail.com>
2026-07-15 20:23:14 +0500

    update

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from "express";
+import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors'
+import 'dotenv/config'
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
