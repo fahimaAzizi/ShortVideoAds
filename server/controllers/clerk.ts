a297db078c183763f95ec775bee46524e4bcbfdd
Fahima Azizi <fahimaazizi397@gmail.com>
2026-07-17 16:15:16 +0500

    update

--- server/controllers/clerk.ts
+++ server/controllers/clerk.ts
@@ -4,7 +4,32 @@ import { verifyWebhook } from "@clerk/express/webhooks";
 const clerkWebhooks = async (req: Request, res: Response) => {
   try {
     const evt: any = await verifyWebhook(req);
-  } catch (error) {
+    const {data, type} =evt;
+    switch (type){
+        case "user.created": {
+            await prisma.user.create({
+                data: {
+                    id:data.id,
+                    email:data?.email_email_addresses[0]?.email_address,
+                    name:data.first_name + " " + data?.last_name,
+                    image: data?.image_url.
+                  }
+            })
+            break;
+        }
+             case "user.created": {
+            await prisma.user.create({
+                data: {
+                    id:data.id,
+                    email:data?.email_email_addresses[0]?.email_address,
+                    name:data.first_name + " " + data?.last_name,
+                    image: data?.image_url.
+                  }
+            })
+            break;
+        }
+    }
+   } catch (error) {
 
   }
 };
