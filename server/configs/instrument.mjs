f4bdcb7267c1d539363a30e828e00e48d1ab2ec3
Fahima Azizi <fahimaazizi397@gmail.com>
2026-07-20 21:06:02 +0500

    update

---
+++ server/configs/instrument.mjs
@@ -0,0 +1,12 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://3c7198bd435bbb9c07f22981c4521078@o4511768457773056.ingest.de.sentry.io/4511768466554960",
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/node/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
