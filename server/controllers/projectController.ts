663b0b08c404c57eb49ee3f8cfa87c167eb3159e
Fahima Azizi <fahimaazizi397@gmail.com>
2026-07-30 16:05:16 +0500

    update

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -135,6 +135,25 @@ export const createProject = async (
   if(!response?/candidates?.[0]?.content?.parts){
     throw new Error('Unexpected response')
   }
+  const parts = response.candidates[0].content?.parts;
+  let finalBuffer: Bffer | null = null
+
+  for (const part of parts) {
+  if (part.inlineData) {
+    finalBuffer = Buffer.from(
+      part.inlineData.data,
+      "base64"
+    );
+  }
+}
+
+if (!finalBuffer) {
+  throw new Error("Failed to generate image");
+}
+
+const base64Image = `data:image/png;base64,${finalBuffer.toString(
+  "base64"
+)}`;
 
   } catch (error: any) {
     Sentry.captureException(error);
